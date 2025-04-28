2a64017fa5dd9ebb0b90de339cbd8e7363347421
JongoSimon <simonjongo@gmail.com>
2025-04-28 09:36:32 -0500

    update

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+
+
+//? for loop
+
+let coders = ["Jongo", "Kolbie", "Seth", "Mike"]
+
+for(let i=0; i < coders.length; i++) {
+    //? DO STUFF
+    console.log(coders [i] + " Loves coding!")
+}
+
+//? FOR OF
+
+let poptarts = ["chocolate", "Strawberry", "Fudge S u n d a e", "Oreos"]
+
+
+for (let x of poptarts) {
+    console.log(x);
+}
